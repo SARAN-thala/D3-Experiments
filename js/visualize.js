63afa5d60dff92056b66d3e98e817918af61611c
Saran <saranrajmalar@gmail.com>
2016-10-26 20:53:57 +0530

    Saran | added both axis lines into the graph

--- js/visualize.js
+++ js/visualize.js
@@ -1,9 +1,9 @@
-var newData = function () {
-    var data = [];
+const newData = function () {
+    const data = [];
     for(var i=0;i<=100;i++){
         data[i] =i;
     }
-    return _.sampleSize(data,10);
+    return _.sampleSize(data,11);
 };
 
 const WIDTH = 1420;
@@ -16,12 +16,13 @@ const INNER_HEIGHT = HEIGHT - (2 * MARGIN);
 const loadChart = function (data) {
 
     d3.select('svg').remove();
+
     let svg = d3.select('.container').append('svg')
         .attr('width', WIDTH)
         .attr('height', HEIGHT);
 
     let xScale = d3.scaleLinear()
-        .domain([0, data.length])
+        .domain([0, data.length-1])
         .range([0, INNER_WIDTH]);
 
     let yScale = d3.scaleLinear()
@@ -29,7 +30,7 @@ const loadChart = function (data) {
         .range([INNER_HEIGHT, 0]);
 
     let xAxis = d3.axisBottom(xScale);
-    let yAxis = d3.axisLeft(yScale)
+    let yAxis = d3.axisLeft(yScale);
 
     let g = svg.append('g')
         .attr('transform', `translate(${MARGIN - 8}, ${MARGIN})`);
@@ -37,10 +38,20 @@ const loadChart = function (data) {
     svg.append('g')
         .attr('transform', `translate(${MARGIN - 10}, ${HEIGHT - MARGIN})`)
         .call(xAxis)
+        .classed('xAxis', true);
+
+    svg.selectAll('.xAxis .tick')
+        .append('line')
+        .attr('x1', 0.5)
+        .attr('y1', 0)
+        .attr('x2', 0.5)
+        .attr('y2', -INNER_HEIGHT)
+        .classed('grid', true);
 
     svg.append('g')
         .attr('transform', `translate(${MARGIN - 10}, ${MARGIN})`)
         .call(yAxis)
+        .classed('yAxis', true);
 
     svg.selectAll('.yAxis .tick')
         .append('line')
@@ -51,8 +62,8 @@ const loadChart = function (data) {
         .classed('grid', true);
 
     let line = d3.line()
-        .x(function (d,i) {return xScale(i)})
-        .y(function (d) {return yScale(d)});
+        .x((d,i) => (xScale(i)))
+        .y(d => (yScale(d)));
 
     g.append('path')
         .classed('data', true)
@@ -60,11 +71,11 @@ const loadChart = function (data) {
 
 };
 
-var updateChart = function () {
-    newData().shift()
-    newData().push(0,_.random(1,100))
+const updateChart = function () {
+    newData().shift();
+    newData().push(0,_.random(1,100));
     loadChart(newData());
-}
+};
 
 window.onload = function() {loadChart(newData())};
 
