e2ab2051a611eb5fbe7e2ed8e96870bad1e9a23a
Saran <saranrajmalar@gmail.com>
2016-10-27 00:31:12 +0530

    Saran | created bar chart
    - pull drawChart fn

--- js/visualize.js
+++ js/visualize.js
@@ -1,42 +1,64 @@
-const newData = function () {
-    const data = [];
-    for(var i=0;i<=100;i++){
-        data[i] =i;
-    }
-    return _.sampleSize(data,11);
-};
+const datas = [];
+const UPPER_LIMIT = 100;
+const LOWER_LIMIT = 0;
+const RANGE=10;
 
-const WIDTH = 1420;
-const HEIGHT = 650;
-const MARGIN = 50;
+const translate = (x, y) => (`translate (${x}, ${y})`);
 
+const WIDTH = 1400;
+const HEIGHT = 360;
+const MARGIN = 30;
 const INNER_WIDTH = WIDTH - (2 * MARGIN);
 const INNER_HEIGHT = HEIGHT - (2 * MARGIN);
 
-const loadChart = function (data) {
+const newData = function () {
+    for(let i=0;i<=RANGE;i++){
+        datas.push(_.random(LOWER_LIMIT,UPPER_LIMIT));
+    }
+};
+newData();
 
-    d3.select('svg').remove();
+const getLastData = function () {
+    datas.push(_.random(0, 100));
+    datas.shift(1);
+    return datas;
+};
 
-    let svg = d3.select('.container').append('svg')
+const drawLineChart = function (svg, line, datas) {
+    let g = svg.append('g')
+        .attr('transform', translate(MARGIN,MARGIN));
+
+    g.append('path')
+        .attr('d', line(datas))
+        .classed('path', true);
+};
+
+var drawBarChart = function (svg, datas, yScale, xScale) {
+    svg.selectAll('rect')
+        .data(datas)
+        .enter()
+        .append('rect')
+        .attr('height', function (d) {
+            return INNER_HEIGHT - yScale(d);
+        })
+        .attr('width', 6)
+        .attr('x', function (d, i) {
+            return xScale(i);
+        })
+        .attr('y', function (d) {
+            return yScale(d);
+        })
+        .attr('transform', translate(MARGIN, MARGIN))
+        .classed('rect', true);
+};
+
+const initializeChart = function (xAxis, yAxis, div) {
+    let svg = d3.select(div).append("svg")
         .attr('width', WIDTH)
         .attr('height', HEIGHT);
 
-    let xScale = d3.scaleLinear()
-        .domain([0, data.length-1])
-        .range([0, INNER_WIDTH]);
-
-    let yScale = d3.scaleLinear()
-        .domain([0, 100])
-        .range([INNER_HEIGHT, 0]);
-
-    let xAxis = d3.axisBottom(xScale);
-    let yAxis = d3.axisLeft(yScale);
-
-    let g = svg.append('g')
-        .attr('transform', `translate(${MARGIN - 8}, ${MARGIN})`);
-
     svg.append('g')
-        .attr('transform', `translate(${MARGIN - 10}, ${HEIGHT - MARGIN})`)
+        .attr('transform', translate(MARGIN, HEIGHT - MARGIN))
         .call(xAxis)
         .classed('xAxis', true);
 
@@ -49,7 +71,7 @@ const loadChart = function (data) {
         .classed('grid', true);
 
     svg.append('g')
-        .attr('transform', `translate(${MARGIN - 10}, ${MARGIN})`)
+        .attr('transform', translate(MARGIN, MARGIN))
         .call(yAxis)
         .classed('yAxis', true);
 
@@ -61,24 +83,37 @@ const loadChart = function (data) {
         .attr('y2', 0.5)
         .classed('grid', true);
 
+    return svg;
+};
+
+const loadChart = function () {
+
+    let xScale = d3.scaleLinear()
+        .domain([0, RANGE])
+        .range([0, INNER_WIDTH]);
+
+    let yScale = d3.scaleLinear()
+        .domain([0, 100])
+        .range([INNER_HEIGHT,0]);
+
+    let xAxis = d3.axisBottom(xScale);
+    let yAxis = d3.axisLeft(yScale);
+
+    let svg = initializeChart(xAxis,yAxis,'#line-chart');
+    let svgBar = initializeChart(xAxis,yAxis,'#bar-chart');
+
     let line = d3.line()
         .x((d,i) => (xScale(i)))
         .y(d => (yScale(d)));
 
-    g.append('path')
-        .classed('data', true)
-        .attr('d', line(data));
+    setInterval(function () {
+        let randomNumbers = getLastData();
+        svg.selectAll('.path').remove();
+        svgBar.selectAll('.rect').remove();
 
+        drawLineChart(svg,line,randomNumbers);
+        drawBarChart(svgBar,randomNumbers,yScale,xScale);
+    },250);
 };
 
-const updateChart = function () {
-    newData().shift();
-    newData().push(0,_.random(1,100));
-    loadChart(newData());
-};
-
-window.onload = function() {loadChart(newData())};
-
-setInterval(function () {
-    updateChart();
-},1000)
+window.onload = loadChart;
