9098f79daa8cfd8fd24a74a7fe2211be02a37154
Saran <saranrajmalar@gmail.com>
2016-11-08 13:32:16 +0530

    Saran | created box size according to their values in box

---
+++ show-numbers/js/showNumbers.js
@@ -0,0 +1,21 @@
+const data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+const createTable = () => {
+
+    let box = d3.select('body').append('div');
+
+    let scale = d3.scaleLinear()
+        .domain([0,10])
+        .range(['italic bold 12px/30px Georgia, serif','italic bold 120px/300px Georgia, serif']);
+
+    box.selectAll('div')
+        .data(data)
+        .enter()
+        .append('div')
+        .style('font', d => (scale(d)))
+        .classed('box',true)
+        .text(d => d)
+};
+
+window.onload = createTable;
