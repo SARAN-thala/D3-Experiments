bfe1486a69fa185f62cee39f7460be2aaca7bbfd
Saran <saranrajmalar@gmail.com>
2016-11-15 14:33:44 +0530

    Saran | #refactor the alignment of shapes

--- simple-shapes/js/simpleShapes.js
+++ simple-shapes/js/simpleShapes.js
@@ -2,29 +2,32 @@ const createShapes = function () {
     const svg = d3.select('div')
         .append('svg')
         .attr('width', 600)
-        .attr('height', 100);
+        .attr('height', 102);
 
-    const slope = svg.append('line')
+    svg.append('line')
         .attr('x1', 110)
-        .attr('y1', 0)
+        .attr('y1', 1)
         .attr('x2', 10)
-        .attr('y2', 100);
+        .attr('y2', 101)
+        .classed('slope',true);
 
-    const circle = svg.append('circle')
+    svg.append('circle')
         .attr('cx',210)
-        .attr('cy',50)
+        .attr('cy',51)
         .attr('r',50);
 
-    const square = svg.append('rect')
+    svg.append('rect')
         .attr('x',310)
-        .attr('y',0)
+        .attr('y',1)
         .attr('rx',5)
         .attr('ry',5)
         .attr('width',100)
-        .attr('height',100);
+        .attr('height',100)
+        .classed('square',true);
 
-    const triangle = svg.append('polygon')
-        .attr('points', "460,100,460,100,510,0,560,100")
+    svg.append('polygon')
+        .attr('points', "460,100,460,100,510,1,560,101")
+        .classed('triangle',true);
 };
 
 window.onload = createShapes;
