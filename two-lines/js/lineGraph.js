8d715473abaa586f9a37ed6d0f45490257cd82e0
Saran <saranrajmalar@gmail.com>
2016-11-17 12:30:44 +0530

    Saran | Added small circles to mark the data points
    - completed ex:08

--- two-lines/js/lineGraph.js
+++ two-lines/js/lineGraph.js
@@ -1,6 +1,6 @@
-const data = [{x: 0, y: 5}, {x: 1, y: 9}, {x: 2, y: 7}, {x: 3, y: 5}, {x: 4, y: 3}, {x: 6, y: 4}, {x: 7, y: 2}, {x: 8, y: 3}, {x: 9, y: 2}];
+const DATA = [{x: 0, y: 5}, {x: 1, y: 9}, {x: 2, y: 7}, {x: 3, y: 5}, {x: 4, y: 3}, {x: 6, y: 4}, {x: 7, y: 2}, {x: 8, y: 3}, {x: 9, y: 2}];
 
-const translate = (x, y) => (`translate (${x}, ${y})`);
+let translate = (x, y) => (`translate (${x}, ${y})`);
 
 const WIDTH = 620;
 const HEIGHT = 620;
@@ -8,18 +8,31 @@ const MARGIN = 30;
 const INNER_WIDTH = WIDTH - (2 * MARGIN);
 const INNER_HEIGHT = HEIGHT - (2 * MARGIN);
 
+let xScale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([0, INNER_WIDTH]);
+
+let yScale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([INNER_HEIGHT, 0]);
+
+const X_SCALE = d => ( xScale(d.x / 10));
+const Y_SCALE = d => ( yScale(d.y / 10));
+const SIN_Y_SCALE = d => (yScale(Math.sin(d.x) / 10 + 0.5));
+
+let generateCircles = function (xVal, yVal, data, conatiner) {
+    conatiner.append('g').selectAll('circle')
+        .data(data)
+        .enter().append('circle')
+        .attr('r', 5)
+        .attr('cx', xVal)
+        .attr('cy', yVal)
+};
+
 const loadChart = function () {
     let svg = d3.select('.container').append('svg')
         .attr('width', WIDTH)
         .attr('height', HEIGHT);
-
-    let xScale = d3.scaleLinear()
-        .domain([0, 1])
-        .range([0, INNER_WIDTH]);
-
-    let yScale = d3.scaleLinear()
-        .domain([0, 1])
-        .range([INNER_HEIGHT, 0]);
 
     let xAxis = d3.axisBottom(xScale);
     let yAxis = d3.axisLeft(yScale);
@@ -33,23 +46,28 @@ const loadChart = function () {
         .call(yAxis);
 
     let g = svg.append('g')
-        .attr('transform', translate(MARGIN,MARGIN));
+        .attr('transform', translate(MARGIN, MARGIN));
 
     let line = d3.line()
-        .x((d) => (xScale(d.x/10)))
-        .y(d => (yScale(d.y/10)));
+        .x(X_SCALE)
+        .y(Y_SCALE);
 
     let sine = d3.line()
-        .x(d => (xScale(d.x/10)))
-        .y(d => (yScale(Math.sin(d.x)/10 + 0.5)));
+        .x(X_SCALE)
+        .y(SIN_Y_SCALE);
 
     g.append('path')
-        .attr('d', line(data))
+        .attr('d', line(DATA))
         .classed('line-path', true);
 
     g.append('path')
-        .attr('d', sine(data))
+        .attr('d', sine(DATA))
         .classed('line-path', true);
+
+    generateCircles(X_SCALE, Y_SCALE, DATA, g);
+    generateCircles(X_SCALE, SIN_Y_SCALE, DATA, g);
+
+    g.selectAll('circle').exit().remove();
 
 };
 
