192dcc1facd06d6f8daf33f62f6b6a57128a8a5e
Saran <saranrajmalar@gmail.com>
2016-11-17 17:45:50 +0530

    Saran | created sine wave
    - using formulae y = (sin(3x)+1)/2 for x: 0 to 9

---
+++ trace-the-path/js/tracePath.js
@@ -0,0 +1,64 @@
+const SIN_DATA = [0,1,2,3,4,5,6,7,8,9];
+
+let translate = (x, y) => (`translate (${x}, ${y})`);
+
+const WIDTH = 620;
+const HEIGHT = 620;
+const MARGIN = 30;
+const INNER_WIDTH = WIDTH - (2 * MARGIN);
+const INNER_HEIGHT = HEIGHT - (2 * MARGIN);
+
+let xScale = d3.scaleLinear()
+    .domain([0, 10])
+    .range([0, INNER_WIDTH]);
+
+let yScale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([INNER_HEIGHT, 0]);
+
+const SIN_X_SCALE = d => ( xScale(d));
+const SIN_Y_SCALE = d => (yScale((Math.sin(3*d)+1)/2));
+
+let generateCircles = function (xVal, yVal, data, conatiner) {
+    conatiner.append('g').selectAll('circle')
+        .data(data)
+        .enter().append('circle')
+        .attr('r', 5)
+        .attr('cx', xVal)
+        .attr('cy', yVal)
+};
+
+const loadChart = function () {
+    let svg = d3.select('.container').append('svg')
+        .attr('width', WIDTH)
+        .attr('height', HEIGHT);
+
+    let xAxis = d3.axisBottom(xScale);
+    let yAxis = d3.axisLeft(yScale);
+
+    svg.append('g')
+        .attr('transform', translate(MARGIN, HEIGHT - MARGIN))
+        .call(xAxis);
+
+    svg.append('g')
+        .attr('transform', translate(MARGIN, MARGIN))
+        .call(yAxis);
+
+    let g = svg.append('g')
+        .attr('transform', translate(MARGIN, MARGIN));
+
+    let sine = d3.line()
+        .x(SIN_X_SCALE)
+        .y(SIN_Y_SCALE);
+
+    g.append('path')
+        .attr('d', sine(SIN_DATA))
+        .classed('line-sine-path', true);
+
+    generateCircles(SIN_X_SCALE, SIN_Y_SCALE, SIN_DATA, g);
+
+    g.selectAll('circle').exit().remove();
+
+};
+
+window.onload = loadChart;
